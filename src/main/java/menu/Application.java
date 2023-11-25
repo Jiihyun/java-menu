4db3baca2bacbd7983b54d589481b72d0b32e203
Jiihyun <cwlgusc@gmail.com>
2023-11-25 21:50:38 +0900

    feat(Application & MenuController): 프로그램 흐름 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,23 @@
 package menu;
 
+import menu.controller.MenuController;
+import menu.domain.CategorySelector;
+import menu.io.reader.ConsoleReader;
+import menu.io.reader.Reader;
+import menu.io.writer.ConsoleWriter;
+import menu.io.writer.Writer;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
 
-  public static void main(final String[] args) {
-  }
+    public static void main(final String[] args) {
+        Reader reader = new ConsoleReader();
+        Writer writer = new ConsoleWriter();
+        InputView inputView = new InputView(reader, writer);
+        OutputView outputView = new OutputView(writer);
+        CategorySelector categorySelector = new CategorySelector();
+        MenuController menuController = new MenuController(inputView, outputView, categorySelector);
+        menuController.start();
+    }
 }
--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,4 +1,35 @@
 package menu.controller;
 
+import menu.domain.*;
+import menu.domain.dto.input.NamesRequest;
+import menu.domain.dto.input.NonEdibleMenuRequest;
+import menu.domain.dto.output.NamesResponse;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CategorySelector categorySelector;
+
+    public MenuController(InputView inputView, OutputView outputView, CategorySelector categorySelector) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.categorySelector = categorySelector;
+    }
+
+    public void start() {
+        NonEdibleMenu nonEdibleMenu = readCoachesData();
+        WeekMenuCategories weekMenuCategories = WeekMenuCategories.from(categorySelector);
+        ResultCategories resultCategories = ResultCategories.from(weekMenuCategories, nonEdibleMenu);
+        outputView.printResult(weekMenuCategories, resultCategories);
+    }
+
+    private NonEdibleMenu readCoachesData() {
+        NamesRequest namesRequest = inputView.readNames();
+        Names names = Names.from(namesRequest.getInputNames());
+        NamesResponse namesResponse = names.toNamesResponse(names);
+        NonEdibleMenuRequest nonEdibleMenuRequest = inputView.readNonEdibleMenu(namesResponse);
+        return NonEdibleMenu.from(nonEdibleMenuRequest);
+    }
 }
